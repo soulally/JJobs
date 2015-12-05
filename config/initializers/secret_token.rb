82f976e44766774639e1f497cf9c1745d12bbdde
JamesWalker <soulally@me.com>
2015-12-05 03:48:57 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jjobs::Application.config.secret_key_base = '776b9d6196e692e2b4dd26138b72c81a0cba8b8acb038ce0925630eb290dbbad05e5ddfb6a8e4890ec04367849318fd6752211960621c418f6a87ff98bfa769b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jjobs::Application.config.session_store :cookie_store, key: '_jjobs_session'
